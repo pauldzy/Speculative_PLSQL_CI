5daee3482ec19dddf0a912a8b4f6c9cd5d0829de
pauldziemiela <pauldziemiela@gmail.com>
2016-01-03 08:06:18 -0500

    restructure supporting items

---
+++ SupportFiles/sqlplus_footer.sql
@@ -0,0 +1,35 @@
+
+SHOW ERROR;
+
+DECLARE
+   l_num_errors PLS_INTEGER;
+
+BEGIN
+
+   SELECT
+   COUNT(*)
+   INTO l_num_errors
+   FROM
+   user_errors a
+   WHERE
+   a.name LIKE 'DZ_DZNAMEDZ%';
+
+   IF l_num_errors <> 0
+   THEN
+      RAISE_APPLICATION_ERROR(-20001,'COMPILE ERROR');
+
+   END IF;
+
+   l_num_errors := DZ_DZNAMEDZ_TEST.inmemory_test();
+
+   IF l_num_errors <> 0
+   THEN
+      RAISE_APPLICATION_ERROR(-20001,'INMEMORY TEST ERROR');
+
+   END IF;
+
+END;
+/
+
+EXIT;
+
